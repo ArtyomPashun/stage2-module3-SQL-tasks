358fc6d4ff77f982e4e54334778ae6638dde8c93
Artsiom Pashun <artyom.pashun@gmail.com>
2023-07-12 14:06:57 +0200

    Additional fixes v.0.2

--- lesson8/src/main/resources/SOLUTION.sql
+++ lesson8/src/main/resources/SOLUTION.sql
@@ -2,6 +2,6 @@ SELECT MAX(birthday) FROM student;
 
 SELECT MIN(payment_date) FROM payment;
 
-SELECT AVG (mark) as a FROM mark HAVING subject_id = (SELECT id FROM subject WHERE name LIKE 'Math') GROUP BY a;
+SELECT AVG (mark) as a FROM mark HAVING subject_id = (SELECT id FROM subject WHERE name LIKE 'Math') GROUP BY subject_id;
 
-SELECT MIN(amount) as m FROM payment HAVING type_id = (SELECT id FROM paymenttype WHERE name LIKE 'WEEKLY') GROUP BY m;
+SELECT MIN(amount) as m FROM payment HAVING type_id = (SELECT id FROM paymenttype WHERE name LIKE 'WEEKLY') GROUP BY type_id;
--- lesson9/src/main/resources/SOLUTION.sql
+++ lesson9/src/main/resources/SOLUTION.sql
@@ -1,6 +1,6 @@
-SELECT * FROM student JOIN mark ON student.id = mark.student_id HAVING AVG(mark) > 8 GROUP BY student.id;
+SELECT * FROM student JOIN mark ON student.id = mark.student_id HAVING AVG(mark) > 8 GROUP BY mark.student_id;
 
-SELECT id, name FROM student JOIN mark on student.id = mark.student_id  HAVING MIN(mark) > 7 GROUP BY student.id;
+SELECT student.id, student.name FROM student JOIN mark on student.id = mark.student_id  HAVING MIN(mark) > 7 GROUP BY mark.student_id;
 
-SELECT id, name FROM student WHERE ID IN (SELECT student_id FROM payment WHERE YEAR(payment_date) = 2019 GROUP BY student_id HAVING count(*) > 2);
+SELECT id, name FROM student WHERE id IN (SELECT student_id FROM payment WHERE YEAR(payment_date) = 2019 GROUP BY student_id HAVING count(*) > 2);
 
