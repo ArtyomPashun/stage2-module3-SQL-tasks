61a523d0b85162af6b149ecb7cd048143d86d734
Artsiom Pashun <artyom.pashun@gmail.com>
2023-07-12 12:52:33 +0200

    Some fixes

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -3,4 +3,5 @@ DELETE FROM student WHERE id IN (SELECT DISTINCT student_id FROM mark JOIN subje
 DELETE FROM student WHERE id IN (SELECT student_id FROM mark GROUP BY student_id HAVING MIN(mark) < 4);
 
 DELETE FROM paymenttype WHERE name = 'DAILY';
+
 DELETE FROM mark WHERE mark < 7;
