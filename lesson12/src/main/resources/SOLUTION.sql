16095c2345721b77e9f01c81ed6f90c91f37ef38
Artsiom Pashun <artyom.pashun@gmail.com>
2023-07-12 14:33:50 +0200

    Additional fixes v.0.3

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
-DELETE FROM student WHERE id IN (SELECT DISTINCT student_id FROM mark JOIN subject s on s.id = mark.subject_id WHERE s.grade >= 4);
+DELETE FROM student WHERE id IN (SELECT DISTINCT student_id FROM mark  INNER JOIN subject s on s.id = mark.subject_id WHERE s.grade >= 4);
 
 DELETE FROM student WHERE id IN (SELECT student_id FROM mark GROUP BY student_id HAVING MIN(mark) < 4);
 
